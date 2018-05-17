b897a80997b256fe34f4de9379bff549b6f1ff1f
LDmora <dmora@geosat.com.co>
2018-05-17 08:17:59 -0500

    integracion de firebase

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
+import './firebase';
+
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+import VueFire from 'vuefire';
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+Vue.use(VueFire)
+
+firebase.initializeApp({
+  projectId: 'firestarter-96e46',
+  databaseURL: 'https://firestarter-96e46.firebaseio.com'
+})
 
 new Vue({
   render: h => h(App)
