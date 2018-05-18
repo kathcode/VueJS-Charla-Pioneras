54ec2ed321dd87953a6150ea40bc23d884d5e2eb
Andrey Ruíz <davkorss@gmail.com>
2018-05-17 21:15:10 -0500

    watch and push example

--- src/main.js
+++ src/main.js
@@ -2,18 +2,11 @@ import './firebase';
 
 import Vue from 'vue'
 import App from './App.vue'
+import VueFire from 'vuefire'
 
 Vue.config.productionTip = false
 
-import VueFire from 'vuefire';
-import firebase from 'firebase/app'
-import 'firebase/firestore'
 Vue.use(VueFire)
-
-firebase.initializeApp({
-  projectId: 'firestarter-96e46',
-  databaseURL: 'https://firestarter-96e46.firebaseio.com'
-})
 
 new Vue({
   render: h => h(App)
